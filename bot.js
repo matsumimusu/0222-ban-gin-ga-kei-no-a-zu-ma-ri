522d95cab72b3bd7f1ae3a266b75a241a22c7519
matsumi <samlin0727@gmail.com>
2022-08-21 14:30:00 +0800

    220821

---
+++ bot.js
@@ -0,0 +1,62 @@
+const {Client} = require('discord.js') ;
+const client = new Client ;
+const key = require('./key.json') ;
+const preifx = require('./preifx.json') ;
+//token
+//client.login(process.env.DISCORD_TOKEN);
+client.login(key[222]);
+client.on('ready',()=>{
+    console.log(`${client.user.tag}安ニャーsay yooo~~~`) ;
+}) ;
+//身份組
+client.on('messageReactionAdd', (reaction, user) => {
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === '969877193146634271') {
+        switch (reaction.emoji.name) {
+            case '🆓':
+                member.roles.add('965668031114129438')
+                break;
+        }}});
+client.on('messageReactionRemove', (reaction, user) => {
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === '969877193146634271') {
+        switch (reaction.emoji.name) {
+            case '🆓':
+                member.roles.remove('965668031114129438')
+                break;
+        }}});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message',msg=>{
+    if(msg.content === (`222`)){
+        msg.channel.send(`**USER : **${msg.author}我在喔`)
+    } ;
+    if(msg.content === (preifx.image)){
+        msg.channel.send(`圖片系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.joke)){
+        msg.channel.send(`笑話系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.level)){
+        msg.channel.send(`伺服器經驗系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.muisic)){
+        msg.channel.send(`音樂系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.tool)){
+        msg.channel.send(`小工具系統(施工中)`)
+    } ;
+})
