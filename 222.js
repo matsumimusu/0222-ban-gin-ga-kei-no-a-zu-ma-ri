e85fe00b04369104121799e88b8dea35d5fc59bd
matsumi <samlin0727@gmail.com>
2022-08-21 13:15:22 +0800

    220821

---
+++ 222.js
@@ -0,0 +1,32 @@
+const {Client} = require('discord.js') ;
+const client = new Client ;
+const key = require('./key.json') ;
+const preifx = require('./preifx.json') ;
+
+
+client.login(key[222]);
+client.on('ready',()=>{
+    console.log(`${client.user.tag}安ニャーsay yooo~~~`) ;
+}) ;
+
+
+client.on('message',msg=>{
+    if(msg.content === (`222`)){
+        msg.channel.send(`**USER : **${msg.author}我在喔`)
+    } ;
+    if(msg.content === (preifx.image)){
+        msg.channel.send(`圖片系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.joke)){
+        msg.channel.send(`笑話系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.level)){
+        msg.channel.send(`伺服器經驗系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.muisic)){
+        msg.channel.send(`音樂系統(施工中)`)
+    } ;
+    if(msg.content === (preifx.tool)){
+        msg.channel.send(`小工具系統(施工中)`)
+    } ;
+})
